e8353b39a84bba5eca9745da53ee429923246004
LeoCholvy <leo.cholvy@gmail.com>
2026-05-09 18:55:43 +0200

    TODO : - ajouter les options (        mask_person: bool = Form(True),
                                   mask_email: bool = Form(True),
    
                                   mask_phone: bool = Form(True),
    
                                   mask_location: bool = Form(True),
    
                                   mask_bank: bool = Form(True)
    
    - Remettre les anciennes routes : download

--- read_pdf.py
+++
@@ -1,7 +0,0 @@
-import sys
-from pypdf import PdfReader
-
-reader = PdfReader('DocAnonymiser.pdf')
-text = '\n'.join([page.extract_text() for page in reader.pages])
-with open('pdf_text.txt', 'w', encoding='utf-8') as f:
-    f.write(text)
